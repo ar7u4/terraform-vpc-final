6723349eef97d846aef302033170f32748645c72
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2023-08-17 23:44:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
+locals {
+  availability_zones = ["${var.aws_region}a", "${var.aws_region}b"]
+}
+
+# VPC
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
@@ -13,6 +29,7 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+# Public subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc.id
   count                   = length(var.public_subnets_cidr)
@@ -26,6 +43,7 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+# Private Subnet
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.vpc.id
   count                   = length(var.private_subnets_cidr)
@@ -39,19 +57,22 @@ resource "aws_subnet" "private_subnet" {
   }
 }
 
+#Internet gateway
 resource "aws_internet_gateway" "ig" {
   vpc_id = aws_vpc.vpc.id
   tags = {
-    Name        = "${var.environment}-igw"
-    Environment = var.environment
+    "Name"        = "${var.environment}-igw"
+    "Environment" = var.environment
   }
 }
 
+# Elastic-IP (eip) for NAT
 resource "aws_eip" "nat_eip" {
   vpc        = true
   depends_on = [aws_internet_gateway.ig]
 }
 
+# NAT Gateway
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
@@ -61,6 +82,7 @@ resource "aws_nat_gateway" "nat" {
   }
 }
 
+# Routing tables to route traffic for Private Subnet
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
   tags = {
@@ -69,6 +91,7 @@ resource "aws_route_table" "private" {
   }
 }
 
+# Routing tables to route traffic for Public Subnet
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.vpc.id
 
@@ -78,18 +101,21 @@ resource "aws_route_table" "public" {
   }
 }
 
+# Route for Internet Gateway
 resource "aws_route" "public_internet_gateway" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.ig.id
 }
 
+# Route for NAT Gateway
 resource "aws_route" "private_internet_gateway" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.nat.id
 }
 
+# Route table associations for both Public & Private Subnets
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnets_cidr)
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
